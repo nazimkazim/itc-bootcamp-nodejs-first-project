a0cb0d3e60270bbc425b0973dc18b80003bb969f
nazimkazim <nazik_111@mail.ru>
2022-10-06 20:51:16 +0600

    add searching in title

--- routes/post.js
+++ routes/post.js
@@ -7,13 +7,15 @@ const router = Router();
 
 router.post("/", checkAuth, async (req, res) => {
   try {
-    const { title, description, img } = req.body;
+    const { title, description, img, tags } = req.body;
     const user = await UserModel.findById(req.user.userId);
     const post = new PostModel({
       title,
       description,
       img,
+      tags: tags.map((tag) => tag.toLowerCase()),
     });
+    console.log(post);
     post.author = user._id;
     await post.save();
     await UserModel.findByIdAndUpdate(user._id, {
@@ -82,15 +84,34 @@ router.get("/view/:id", async (req, res) => {
   }
 })
 
+router.get("/user/:userId", checkAuth, async (req, res) => {
+  try {
+      const post = await PostModel.find({ author: req.params.userId });
+      res.status(200).json(post);
+  } catch (error) {
+      res.status(400).send({ error: error.message });
+  }
+});
+
 router.get("/s/tags", async (req, res) => {
   try {
-    const tags = req.query.tags.split(",");
-    console.log(tags);
+    const tags = req.query.tags.split(",").map((tag) => tag.toLowerCase());
     const posts = await PostModel.find({tags: {$in: tags }});
     res.status(200).send({posts});
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
 })
+
+router.get("/s/search", async (req, res) => {
+  const search = req.query.q;
+  try {
+    const posts = await PostModel.find({title: {$regex: search, $options: "i"}});
+    res.status(200).send({posts});
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+})
+
 
 export default router;
