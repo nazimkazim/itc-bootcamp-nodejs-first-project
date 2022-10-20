d915d6b75b65cfd97ed90423abbb83b7f90c64b5
nazimkazim <nazik_111@mail.ru>
2022-10-20 16:15:37 +0600

    add post

--- routes/post.js
+++ routes/post.js
@@ -19,7 +19,7 @@ router.post("/", checkAuth, async (req, res) => {
     post.author = user._id;
     await post.save();
     await UserModel.findByIdAndUpdate(user._id, {
-        $push: { posts: post },
+      $push: { posts: post },
     });
     res.status(201).json({ message: "Post created" });
   } catch (error) {
@@ -28,90 +28,123 @@ router.post("/", checkAuth, async (req, res) => {
 });
 
 router.get("/", async (req, res) => {
-    try {
-        const post = await PostModel.find().populate("author");
-        res.status(200).json(post);
-    } catch (error) {
-        res.status(400).send({ error: error.message });
-    }
-} );
+  try {
+    const post = await PostModel.find().populate("author");
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
 
 router.get("/:id", async (req, res) => {
-    try {
-        const post = await PostModel.findById(req.params.id).populate("author").populate("comments");
-        res.status(200).json(post);
-    } catch (error) {
-        res.status(400).send({ error: error.message });
-    }
-})
+  try {
+    const post = await PostModel.findById(req.params.id)
+      .populate("author")
+      .populate("comments");
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
 
-router.delete("/:id", checkAuth, async (req, res) => {
-    try {
-      const post = await PostModel.findById(req.params.id);
-      if (post.author.toString() !== req.user.userId) {
-        return res.status(401).json({ message: "Unauthorized" });
-      }
-      await PostModel.findByIdAndDelete(req.params.id);
-      const user = await UserModel.findById(req.user.userId);
-      user.posts.splice(user.posts.indexOf(req.params.id), 1);
-      await user.save();
-      res.status(200).json({ message: "Post deleted" });
-    } catch (error) {
-      res.status(400).send({ error: error.message });
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await PostModel.findById(req.params.id);
+    if (post.author.toString() !== req.user.userId) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
-})
+    await PostModel.findByIdAndDelete(req.params.id);
+    const user = await UserModel.findById(req.user.userId);
+    user.posts.splice(user.posts.indexOf(req.params.id), 1);
+    await user.save();
+    res.status(200).json({ message: "Post deleted" });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
 
 router.put("/:id", checkAuth, async (req, res) => {
   try {
     const post = await PostModel.findById(req.params.id);
     if (post.author.toString() == req.user.userId) {
       await PostModel.findByIdAndUpdate(req.params.id, req.body);
-      res.status(200).json({message: "Post updated"});
+      res.status(200).json({ message: "Post updated" });
     }
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
-})
+});
 
 router.get("/view/:id", async (req, res) => {
   try {
     await PostModel.findByIdAndUpdate(req.params.id, {
-      $inc: {views: 1}
-    })
-    res.status(200).json({message: "View added"});
+      $inc: { views: 1 },
+    });
+    res.status(200).json({ message: "View added" });
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
-})
+});
 
 router.get("/user/:userId", checkAuth, async (req, res) => {
   try {
-      const post = await PostModel.find({ author: req.params.userId });
-      res.status(200).json(post);
+    const post = await PostModel.find({ author: req.params.userId });
+    res.status(200).json(post);
   } catch (error) {
-      res.status(400).send({ error: error.message });
+    res.status(400).send({ error: error.message });
   }
 });
 
 router.get("/s/tags", async (req, res) => {
   try {
     const tags = req.query.tags.split(",").map((tag) => tag.toLowerCase());
-    const posts = await PostModel.find({tags: {$in: tags }});
-    res.status(200).send({posts});
+    const posts = await PostModel.find({ tags: { $in: tags } });
+    res.status(200).send({ posts });
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
-})
+});
 
 router.get("/s/search", async (req, res) => {
   const search = req.query.q;
   try {
-    const posts = await PostModel.find({title: {$regex: search, $options: "i"}});
-    res.status(200).send({posts});
+    const posts = await PostModel.find({
+      title: { $regex: search, $options: "i" },
+    });
+    res.status(200).send({ posts });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+router.put("/like/:postId", checkAuth, async (req, res) => {
+  const userId = req.user.userId;
+  const postId = req.params.postId;
+  try {
+    if (postId) {
+      await PostModel.findByIdAndUpdate(postId, {
+        $addToSet: { likes: userId },
+      });
+    }
+    res.status(200).json({ message: "Like added" });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+router.put("/unlike/:postId", checkAuth, async (req, res) => {
+  const userId = req.user.userId;
+  const postId = req.params.postId;
+  try {
+    if (postId) {
+      await PostModel.findByIdAndUpdate(postId, {
+        $pull: { likes: userId },
+      })
+    }
+    res.status(200).json({ message: "Like removed" });
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
 })
-
 
 export default router;
